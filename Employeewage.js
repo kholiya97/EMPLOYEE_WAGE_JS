013c9ce0c56e0b0e8ebcf943b23fec850637b1bc
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-05-31 22:21:06 +0530

    Added file of UC2_CALC_EMP_WAGE

--- Employeewage.js
+++ Employeewage.js
@@ -1,15 +1,36 @@
-console.log("welcome to employee wage computation problem");
-console.log("UC1");
-//UC1
+console.log("Welcome to Employee Wage Problem \n")
+//UC1 Ability to check Employees is present or absent.
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random()*10)%2;
+if(empCheck == IS_ABSENT)
 {
-let checkForEmployeePresence= Math.floor((Math.random()*10)%2);
-const IS_ABSENT=0;
-if(checkForEmployeePresence==IS_ABSENT)
+    console.log("Employee is absent");
+}
+else 
 {
-    console.log("employee is absent");
+    console.log("Employee is present");
 }
-else
+//UC2 Ability to Calculate Daily Employee Wage based on part time or full time work.
+console.log("UC2 Calculating wage for employee");
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS =4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+let empHours = 0;
+switch(empCheck) //Calculate wage 
 {
-    console.log("employee is present");
+    case IS_PART_TIME:
+    console.log("Employee is working part time");
+    empHours=PART_TIME_HOURS;
+    break;
+    case IS_FULL_TIME:
+    console.log("Employee is working full time");
+    empHours = FULL_TIME_HOURS;
+    break;
+    default:
+    empHours =0;
+    break;
 }
-}
+let empWage = empHours*WAGE_PER_HOUR;
+console.log("Employee wage: " + empWage);
