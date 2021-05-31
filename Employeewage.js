3b1c55001b90e5b28ae4da64935b874f525fb027
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-05-31 22:51:16 +0530

    Added file of UC5_CALC_WAGE_USING_CONDITION

--- Employeewage.js
+++ Employeewage.js
@@ -1,57 +1,67 @@
-console.log("welcome to employee wage computation problem");
-console.log("UC1");
-//UC1
+console.log("Welcome to Employee Wage Problem \n")
+//UC1 Ability to check Employees is present or absent.
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random()*10)%2;
+if(empCheck == IS_ABSENT)
 {
-let checkForEmployeePresence= Math.floor((Math.random()*10)%2);
-const IS_ABSENT=0;
-if(checkForEmployeePresence==IS_ABSENT)
-{
-    console.log("employee is absent");
+    console.log("Employee is absent");
 }
-else
+else 
 {
-    console.log("employee is present");
-}
+    console.log("Employee is present");
 }
 
-//UC2
-//Calculating Daily Wage
-//const IS_ABSENT=0;
-const IS_ABSENT=0;
-const WAGE_PER_HOUR=20;
-const TOTAL_WORKING_DAYS=20;
-let totalEmpHours=0;
-let employeeWage=0;
-for(i=0;i<TOTAL_WORKING_DAYS;i++)
+//UC2 Ability to Calculate Daily Employee Wage based on part time or full time work.
 {
-let emp_hours= getEmpHours();
-totalEmpHours+=emp_hours;
-employeeWage= WAGE_PER_HOUR*totalEmpHours;
-}
-console.log("total work hours "+totalEmpHours);
-console.log("totol emp wage "+employeeWage);
-console.log("total working days "+TOTAL_WORKING_DAYS);
-//UC3
-function getEmpHours()
-{ 
-    let emp_hours=0;
-    let checkForEmployeePresence= Math.floor((Math.random()*10)%3);
-    switch(checkForEmployeePresence)
+console.log("UC2 Calculating wage for employee");
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS =4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+let empHours = 0;
+let empCheck = Math.floor(Math.random()*10)%3;
+switch(empCheck)
 {
-    case 1:
-        //console.log("employee is present for part time");
-        emp_hours=4;
-        return emp_hours;
-    case 2:
-        //console.log("employee is present for full time");
-        emp_hours=8;
-        return emp_hours;
+    case IS_PART_TIME:
+    console.log("Employee is working part time");
+    empHours=PART_TIME_HOURS;
+    break;
+    case IS_FULL_TIME:
+    console.log("Employee is working full time");
+    empHours = FULL_TIME_HOURS;
+    break;
     default:
-       // console.log("employee is absent");
-        return emp_hours;
+    empHours =0;
+    break;
 }
+let empWage = empHours*WAGE_PER_HOUR;
+console.log("Employee wage: " + empWage);
 }
-//UC4 Calculating total emp wage for a month assuming 20 working day in a month.
+//UC3 Refactor the UC2 Code to write a function to get work hours.
+  console.log("UC3 Refactor the Code to write a function to get work hours");
+  const IS_PART_TIME = 1;
+  const IS_FULL_TIME = 2;
+  const PART_TIME_HOURS =4;
+  const FULL_TIME_HOURS = 8;
+  const WAGE_PER_HOUR = 20;
+  function getWorkingHrs(empCheck)
+  {
+      switch (empCheck) 
+      {
+          case IS_PART_TIME:
+              return PART_TIME_HOURS;          
+          case IS_FULL_TIME:
+              return FULL_TIME_HOURS;
+          default:       
+              return 0;
+      }
+  }
+  
+  let empWage = WAGE_PER_HOUR*getWorkingHrs(empCheck);
+  console.log("Employee wage: " + empWage);
+
+  //UC4 Calculating total emp wage for a month assuming 20 working day in a month.
 console.log("UC4 Calculating total emp wage for a month assuming 20 working day in a month")
 const NUM_OF_WORKING_DAYS =20;
 let empHours=0;
@@ -62,3 +72,18 @@ for(let day =0; day<NUM_OF_WORKING_DAYS; day++)
 }
 empWage = WAGE_PER_HOUR*empHours;
 console.log("Total working hours in mmonth: "+empHours+" Total monthly employee wage : " + empWage);
+
+ //UC5 Calculating Wages til a condition of total working hours of 160 or max days of 20 is reached for a month.
+ console.log("UC5 Calculating monthly wage given condition")
+ const MAX_HOURS_IN_MONTHS=160;
+ const MAX_NUMBER_OF_DAYS=20;
+ let totalEmpHours=0;
+ let totalWorkingDays=0;
+ while(totalEmpHours<MAX_HOURS_IN_MONTHS && totalWorkingDays<MAX_NUMBER_OF_DAYS)
+ {
+     totalWorkingDays++;
+     let empCheck = Math.floor(Math.random()*10)%3;
+     totalEmpHours += getWorkingHrs(empCheck);
+ }
+ let totalEmpWage = WAGE_PER_HOUR*totalEmpHours;
+ console.log("Total working days  : "+totalWorkingDays+"\nTotal working hours : "+totalEmpHours+" \nTotal employee wage : " + totalEmpWage);
