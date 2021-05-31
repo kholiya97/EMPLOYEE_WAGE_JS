d50aa23905e53a11d5b1e5f4194051cae8661b95
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-05-31 20:43:59 +0530

    Added file of UC1_EMP_PRESENT_OR_ABSENT

---
+++ Employee.js
@@ -0,0 +1,15 @@
+console.log("welcome to employee wage computation problem");
+console.log("UC1");
+//UC1
+{
+let checkForEmployeePresence= Math.floor((Math.random()*10)%2);
+const IS_ABSENT=0;
+if(checkForEmployeePresence==IS_ABSENT)
+{
+    console.log("employee is absent");
+}
+else
+{
+    console.log("employee is present");
+}
+}
